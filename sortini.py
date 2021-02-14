d607afaa2013fc05fe571d7569514fbaf93109ba
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2021-02-14 16:00:16 +0100

    fixed the open(filename)

--- sortini.py
+++ sortini.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: cp1250 -*-
 __version__ = '$Id: sort_ini.py 543 2008-12-19 13:44:59Z mn $'
 
@@ -10,37 +10,37 @@ import sys
 USAGE = 'USAGE:\n\tsort_ini.py file.ini'
 
 def sort_ini(fname):
-	"""sort .ini file: sorts sections and in each section sorts keys"""
-	f = file(fname)
-	lines = f.readlines()
-	f.close()
-	section = ''
-	sections = {}
-	for line in lines:
-		line = line.strip()
-		if line:
-			if line.startswith('['):
-				section = line
-				continue
-			if section:
-				try:
-					sections[section].append(line)
-				except KeyError:
-					sections[section] = [line, ]
-	if sections:
-		sk = list(sections.keys())
-		sk.sort()
-		for k in sk:
-			vals = sections[k]
-			vals.sort()
-			print(k)
-			print('\n'.join(vals))
-			print()
+    """sort .ini file: sorts sections and in each section sorts keys"""
+    f = open(fname)
+    lines = f.readlines()
+    f.close()
+    section = ''
+    sections = {}
+    for line in lines:
+        line = line.strip()
+        if line:
+            if line.startswith('['):
+                section = line
+                continue
+            if section:
+                try:
+                    sections[section].append(line)
+                except KeyError:
+                    sections[section] = [line, ]
+    if sections:
+        sk = list(sections.keys())
+        sk.sort()
+        for k in sk:
+            vals = sections[k]
+            vals.sort()
+            print(k)
+            print('\n'.join(vals))
+            print()
 
 
 if '--version' in sys.argv:
-	print(__version__)
+    print(__version__)
 elif len(sys.argv) < 2:
-	print(USAGE)
+    print(USAGE)
 else:
-	sort_ini(sys.argv[1])
+    sort_ini(sys.argv[1])
